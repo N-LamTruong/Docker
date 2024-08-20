d59f39d02ea218feb484b77037822d66f3433aaa
LamTruong-SysOps <nguyenlamtruong12072001@gmail.com>
2024-08-20 17:24:27 +0700

    shell scirpt install docker + dockrer-compose

---
+++ install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#1.Them khoa GPG chinh thuc cua Docker
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+#2. Them kho luu tru vao apt
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+#3. Cai dat docker
+sudo apt install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+
+#4. Cai dat docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#5. Kiem tra thong tin
+docker --version
+docker-compose --version
